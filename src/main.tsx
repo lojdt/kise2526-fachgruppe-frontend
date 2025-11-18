2a5addf4fbfd660a6d67eb71a4951fd9cd8d0275
DrMORE1702 <tobias.thoene001@stud.fh-dortmund.de>
2025-11-18 12:01:11 +0100

    Refactor React import and use StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
+// Note: mock backend is initialized in App.tsx (initializeMockBackend).
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
